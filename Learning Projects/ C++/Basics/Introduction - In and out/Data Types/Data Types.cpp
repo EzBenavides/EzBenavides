28513b59e21722bbfc5742bdc317964a8011fb6e
EzBenavides <benavidesgermanezequiel@gmail.com>
2024-05-16 12:23:27 -0300

    Create Data Types.cpp

---
+++ Learning Projects/ C++/Basics/Introduction - In and out/Data Types/Data Types.cpp
@@ -0,0 +1,22 @@
+// Basic data types in c++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+  int entero = 15;
+  cout<<entero;
+
+  float flotante = 10.45;
+  cout<<flotante;
+
+  double mayor = 16.4164;
+  cout<< mayor;
+
+  char letra = 'a';
+  cout<<letra;
+  
+  return 0;
+}
