6dab58fd5461ad9190f95951cda7c5005b1b34e5
EzBenavides <benavidesgermanezequiel@gmail.com>
2024-05-16 12:30:11 -0300

    Create Data entry.cpp

---
+++ Learning Projects/ C++/Basics/Introduction - In and out/Data entry/Data entry.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int numero; //definiendo variable
+
+  cout<<"Digite un numero: ";
+  cin>>numero<<endl; //guardando variable
+  
+  cout<<"El numero escrito es: "<<numero;
+
+/* 
+  a partir de ahora arrancamos 
+  con algunos problemas basicos
+*/
+  
+  return 0;
+}
